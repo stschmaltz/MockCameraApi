3204f7f5a89bce36a4acdbd3349a104207796dca
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-04 19:28:03 -0700

    implement :id route on camera with pagination
    Not sure how you normally do pagination on an array field specifically but this feels ok

--- routes/camera-router.js
+++ routes/camera-router.js
@@ -32,4 +32,46 @@ cameraRouter
     console.log(resp);
   });
 
+cameraRouter.use('/:camera_id', (req, res, next) => {
+  console.log('I run first');
+  next();
+});
+
+cameraRouter.route('/:camera_id').get((req, res) => {
+  console.log(req.params.camera_id);
+  console.log(req.query);
+  const queryParams = extractPaginationQueryParams(req.query);
+  console.log(queryParams);
+  Camera.findById(
+    req.params.camera_id,
+    { images: { $slice: [queryParams.offset, queryParams.pageSize] } },
+    (err, cameras) => {
+      // console.log(cameras);
+      console.log(err);
+      if (err) {
+        res.status(500).send(`An error has occurred: ${err}`);
+        return 'nay';
+      }
+      if (!req.timedOut && !res.headersSent) res.json(cameras);
+    },
+  );
+});
+
+const extractPaginationQueryParams = query => {
+  console.log('WE IN', query);
+  const maxPageSize = 10;
+  const { pageSize, pageCount } = query;
+  console.log(pageSize, pageCount);
+  if (!pageSize || !pageCount) {
+    return {
+      pageSize: maxPageSize,
+      offset: 0,
+    };
+  }
+
+  return {
+    pageSize: parseInt(pageSize),
+    offset: pageCount * (pageSize || 0),
+  };
+};
 export default cameraRouter;
