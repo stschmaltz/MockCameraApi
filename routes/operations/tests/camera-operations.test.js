ca247b88de39769f9fd1229bfdf18429cc4baad0
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-20 19:15:33 -0700

    Update camera-operations.test.js

--- routes/operations/tests/camera-operations.test.js
+++ routes/operations/tests/camera-operations.test.js
@@ -1,6 +1,3 @@
-// Please see my comment on camera-operations.test.js for why I don't have real tests here
-// TLDR: I couldn't sort out mocking for some reason (I've mocked in jest many times)
-// after a series of attempts so I aborted and wrote test descriptions as a way to show some sort of test thought process
 describe('common-operations', () => {
   describe('getCameraImageCount', () => {
     it('calls aggregate with expected parameters', () => {});
