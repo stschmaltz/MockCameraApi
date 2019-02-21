cb5beeb0e53155f0067064fbd8e330de61354617
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-20 19:14:42 -0700

    Update camera-router.test.js

--- routes/tests/camera-router.test.js
+++ routes/tests/camera-router.test.js
@@ -1,34 +1,3 @@
-// import cameraRouter, { extractPaginationQueryParams } from '../camera-router';
-// import request from 'supertest';
-// import express from 'express';
-
-// Hey so you'll notice I don't have any real tests. I just simply could not get my mock to work, so I couldn't mock out the operations.
-// This was a big bummer but I figured I would leave my final attempt at mocking in comments (to show it's the same as https://jestjs.io/docs/en/es6-class-mocks)
-// and yeah I tried all 4 of the ways they said.
-// I also read a handful of blogs and I can't see what's different about mine..
-// I decided I might as well show the test descriptions for tests that I would have written. At least you can see my test thought process that way...
-
-// const cameraOperationsMock = jest.mock('../operations/camera-operations');
-// jest.setTimeout(20000); // 20 seconds
-// describe('camera-router', () => {
-//   const app = express();
-//   app.use('/camera/', cameraRouter);
-//   describe('/camera/', () => {
-//     describe('GET', () => {
-//       describe('no query params', () => {
-//         it('should call getListOfCameras', async () => {
-//            This was going to be the general idea of the tests. call each endpoint with all query params that lead to code paths and assert the correct output and functions were called
-//            const response = await request(app).get('/camera/');
-//            expect(response.statusCode).toBe(200);
-//            expect(response.data).toBe({I would have mocked the response in the mock and then had the correct expected output here});
-//            expect(cameraOperationsMock.getListOfCameras).toHaveBeenCalledTimes(1);
-//            expect(cameraOperationsMock.getListOfCameras).toHaveBeenCalledWith(req, res);
-//         });
-//       });
-//     });
-//   });
-// });
-
 describe('camera-router', () => {
   describe('/', () => {
     describe('GET', () => {
