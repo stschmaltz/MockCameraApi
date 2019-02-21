ad1fa3394f54ade5788710666c21d19b0eff014f
Shane Schmaltz <stschmaltz@gmail.com>
2019-02-20 19:15:03 -0700

    Update camera-model.test.js

--- models/tests/camera-model.test.js
+++ models/tests/camera-model.test.js
@@ -1,8 +1,3 @@
-// Please see my comment on camera-operations.test.js for why I don't have real tests here
-
-// to be honest, I don't know if I would have written these tests for this interview anyways
-// I think they are important, but I already spent too long on this test with an over-architected solution
-
 describe('cameraModel', () => {
   it('can create new CameraModel instance with all fields', () => {});
   it('can not create  when camera_id is not present', () => {});
